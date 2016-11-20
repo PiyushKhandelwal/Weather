2b483dfec9541e25d10cb6e50defd065e63e4dc9
PiyushKhandelwal <piyush.khandelwal105@gmail.com>
2016-11-21 00:32:41 +0100

    Add toggle of navbar

--- app/components/NavBox.jsx
+++ app/components/NavBox.jsx
@@ -1,16 +1,23 @@
 var React = require("react");
 var {Link, IndexLink} = require("react-router");
 
+require('bootstrap/dist/js/bootstrap.min.js');
 
 var NavBox = (props) => {
 	return (
-		<div className="container" style={{paddingTop: '20px',paddingBottom:'20px'}}>
+		<div className="container" style={{paddingBottom:"20px",paddingTop:'20px'}}>
 				<nav className="navbar navbar-inverse">
-				 <div className="container-fluid">
+				 <div className="container">
 				 	<div className="navbar-header">
+				 		<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        					<span className="sr-only">Toggle navigation</span>
+        					<span className="icon-bar"></span>
+        					<span className="icon-bar"></span>
+        					<span className="icon-bar"></span>
+      					</button>
 				 		<a className="navbar-brand" href="#">React Weather</a>
 				 	</div>
-				 	<div className="collapse navbar-collapse">
+				 	<div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				 		<ul className="nav navbar-nav">
 				 			<li>
 				 				<IndexLink to="/" activeClassName="active">Weather</IndexLink>
